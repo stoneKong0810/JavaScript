54f6d40a355fb3739c05c0f3b1567a2f96d06e27
stoneKong0810 <stonekong2115@gmail.com>
2025-02-02 23:12:24 +0900

    폼 변경 감지 테스트

--- js/tab.js
+++ js/tab.js
@@ -14,11 +14,24 @@ let car = { name: 'BMW X6', price: [100000, 200000, 300000] };
 
 $('.form-select')
   .eq(0)
-  .on('input', function (e) {
-    let value = e.currentTarget.value;
-    if (value === '셔츠') {
-      $('.form-select').eq(1).removeClass('form-hide');
-    } else if (value === '모자') {
+  .on('input', function () {
+    let value = this.value;
+    let pants = [28, 30, 32, 34, 36];
+    let shirts = ['S', 'M', 'L'];
+
+    if (value === '모자') {
       $('.form-select').eq(1).addClass('form-hide');
+    } else if (value === '셔츠') {
+      $('.form-select').eq(1).removeClass('form-hide');
+      $('.form-select').eq(1).html('');
+      shirts.forEach(function (item) {
+        $('.form-select').eq(1).append(`<option>${item}</option>`);
+      });
+    } else if (value === '바지') {
+      $('.form-select').eq(1).removeClass('form-hide');
+      $('.form-select').eq(1).html('');
+      pants.forEach(function (item) {
+        $('.form-select').eq(1).append(`<option>${item}</option>`);
+      });
     }
   });
